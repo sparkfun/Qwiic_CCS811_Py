9cf3d52095f76608fa6b9b87c440e02ee834f117
Kirk <blowme@cow.com>
2019-07-04 12:13:42 -0600

    fix version level

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.02',
+    version='0.1.03',
 
     description='SparkFun Electronics qwiic CCS811',
     long_description=long_description,
@@ -62,7 +62,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
